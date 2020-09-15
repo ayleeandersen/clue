30f57611548fc8df333cb6c1e948ff2111275611
Aylee <ayleeandersen@gmail.com>
2020-09-14 19:05:21 -0600

    Finished adding clue card on correct submission.

--- src/pages/EnterCode.js
+++ src/pages/EnterCode.js
@@ -19,16 +19,27 @@ class EnterCode extends React.Component {
     if (this.inputRef.current.textInput[0]) {
       this.inputRef.current.textInput[0].focus();
     }
-    for (let i = 0; i < 5; i ++) {
+    for (let i = 0; i < 5; i++) {
       this.inputRef.current.state.input[i] = '';
     }
   }
 
+  addToLocalStorage = clue => {
+    const existing = JSON.parse(localStorage.getItem('discoveredClues')) || [];
+    const alreadyExisting = existing.filter(c => c.person === clue.person).length;
+    if (!alreadyExisting) {
+      existing.push(clue)
+    }
+    localStorage.setItem('discoveredClues', JSON.stringify(existing));
+  }
+
   submit = () => {
     const { code } = this.state;
-    if (CODES.includes(code.toLowerCase())) {
-      this.setState({ code: '', clues: CLUES[code] });
+    const finalCode = code.toLowerCase()
+    if (CODES.includes(finalCode)) {
+      this.setState({ code: '', clues: CLUES[finalCode] });
       this.clearInput();
+      this.addToLocalStorage(CLUES[finalCode]);
     } else {
       this.setState({ valid: false, clues: null });
       this.inputRef.current.textInput[0].focus();
@@ -64,12 +75,21 @@ class EnterCode extends React.Component {
           </>
         )}
         {clues && (
-          <>
-            <h1 className='result'>Correct</h1>
-            <p>{clues.person}</p>
-            <p>{clues.weapon}</p>
-            <p>{clues.place}</p>
-          </>
+          <div className='clue-card'>
+            <h1>Clue</h1>
+            <h3 className='clue-card-row'>
+              <span>Suspect:</span>
+              <span className='clue-card-clue'>{clues.person}</span>
+            </h3>
+            <h3 className='clue-card-row'>
+              <span>Weapon:</span>
+              <span className='clue-card-clue'>{clues.weapon}</span>
+            </h3>
+            <h3 className='clue-card-row'>
+              <span>Location:</span>
+              <span className='clue-card-clue'>{clues.place}</span>
+            </h3>
+          </div>
         )}
       </div>
     );
